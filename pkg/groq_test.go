140ebb4f135f7f4d8e1fb05afb9ac35ce42cb660
0xakash9 <devakash256@gmail.com>
2024-08-18 23:30:33 +0530

    more test

--- pkg/groq_test.go
+++ pkg/groq_test.go
@@ -1,6 +1,7 @@
 package pkg
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -22,4 +23,29 @@ func TestChat(t *testing.T) {
 		t.Errorf("Expected error message '%s', but got '%s'", expectedErrorMsg, err.Error())
 	}
 
+}
+
+func TestGetAllModelValues(t *testing.T) {
+	groq := Groq{
+		ApiKey: "",
+		Model:  Gemma2_9b_it,
+	}
+
+	models := groq.GetModels()
+
+	expectedValue := []GroqModel{
+		Gemma2_9b_it,
+		Gemma_7b_it,
+		Llama_31_70b_versatile,
+		Llama_31_8b_instant,
+		Llama3_70b_8192,
+		Llama3_8b_8192,
+		Llama_guard_3_8b,
+		Mixtral_8x7b_32768,
+		Whisper_large_v3,
+	}
+
+	if !reflect.DeepEqual(expectedValue, models) {
+		t.Errorf("Expected error message '%s', but got '%s'", expectedValue, models)
+	}
 }
